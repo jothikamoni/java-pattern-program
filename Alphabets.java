560eb054115465af2b30ee3afbdfc0867eb42301
inline <jothika11106@gmail.com>
2026-01-26 21:54:51 +0530

    upload

---
+++ Alphabets.java
@@ -0,0 +1,63 @@
+class Alphabets
+{
+	public static void main(String[] args) 
+	{
+		for(char i='A';i<='E';i++){
+		for(char j='A';j<='E';j++){
+			System.out.print(i+ " ");
+		}
+System.out.println();
+	}
+System.out.println();
+	/*
+	A A A A A
+B B B B B
+C C C C C
+D D D D D
+E E E E E
+*/
+for(char i='A';i<='E';i++){
+		for(char j='A';j<='E';j++){
+			System.out.print(j+ " ");
+		}
+System.out.println();
+	}
+System.out.println();
+	/*
+	
+A B C D E
+A B C D E
+A B C D E
+A B C D E
+A B C D E
+*/
+for(char i='E';i>='A';i--){
+		for(char j='E';j>='A';j--){
+			System.out.print(i+ " ");
+		}
+System.out.println();
+	}
+System.out.println();
+/*
+
+E E E E E
+D D D D D
+C C C C C
+B B B B B
+A A A A A
+*/
+for(char i='E';i>='A';i--){
+		for(char j='E';j>='A';j--){
+			System.out.print(j+ " ");
+		}
+System.out.println();
+	}
+System.out.println();
+/*
+
+E D C B A
+E D C B A
+E D C B A
+E D C B A
+E D C B A
+*/
